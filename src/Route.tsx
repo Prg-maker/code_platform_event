b922d27da4ddc88791a85ec5ba19de5d0f761f19
Prg-maker <df3296709@gmail.com>
2022-06-24 09:41:19 -0300

    add form

--- src/Route.tsx
+++ src/Route.tsx
@@ -1,9 +1,11 @@
 import {Routes , Route} from 'react-router-dom'
 import { Evento } from './pages/Evento'
+import { Subscriber } from './pages/Subscribe'
 
 export function Router(){
   return(
     <Routes>
+      <Route path="/" element={<Subscriber/>}/>
       <Route path="/event/lesson/:slug" element={<Evento/>}/>
       <Route path="/event" element={<Evento/>}/>
     </Routes>
